f2be4e690fd093c1b13ab68bb3c23c370f6af8b8
Danny <dannybanko@gmail.com>
2021-11-08 11:08:28 -0800

    basic practice with importing and using node modules

---
+++ Intro-to-Node/index.js
@@ -0,0 +1,9 @@
+const superheroes = require("superheroes");
+const supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySupervillain = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySupervillain);
+
